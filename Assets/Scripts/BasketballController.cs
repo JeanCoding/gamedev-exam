7205cb8b71a46caa5888ff8f29f2f2fb80b36dbc
Jean Kalo <jean.kalo@student.rocva.nl>
2023-06-04 18:10:28 +0200

    Add: basic functionality for the character such as dribbling & holding the ball above your head.

--- Assets/Scripts/BasketballController.cs
+++ Assets/Scripts/BasketballController.cs
@@ -2,17 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class BasketballController : MonoBehaviour {
+    
+    // Create References
+
+    public float MoveSpeed = 10;
+    public Transform Ball;
+    public Transform Arms;
+    public Transform PosOverHead;
+    public Transform PosDribble;
+    public Transform Target;
+
+    // Create Variables
+
+    private bool IsBallInHands = true;
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
+        // Walking
+        Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        transform.position += direction * MoveSpeed * Time.deltaTime;
+        transform.LookAt(transform.position + direction);
+
+        // Ball is in your hands
+        if(IsBallInHands) {
+
+            if (Input.GetKey(KeyCode.Space)) {
+                Ball.position = PosOverHead.position;
+                Arms.localEulerAngles = Vector3.right * 180;
+
+                // Looks towards the basket 
+                transform.LookAt(Target.parent.position);
+            } else {
+                Ball.position = PosDribble.position + Vector3.up * Mathf.Abs(Mathf.Sin(Time.time * 5));
+                Arms.localEulerAngles = Vector3.right * 0;
+            }
+        }
     }
 }
